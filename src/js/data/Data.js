357b9f4b4a24ac119a6846104884604e2db96a9e
Kemo Jallow <kemojal@users.noreply.github.com>
2019-01-27 16:57:12 +0800

    commit

---
+++ src/js/data/Data.js
@@ -0,0 +1,49 @@
+const data = {
+    "currencies": [
+        {
+            "code": "TWD",
+            "sellRate": 1,
+            "name": "Taiwanese Dollars",
+            "sign": "TWD"
+        },
+        {
+            "code": "USD",
+            "sellRate": 0.0325,
+            "name": "United States Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "CAD",
+            "sellRate": 0.0429,
+            "name": "Canadian Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "AUD",
+            "sellRate":0.0452,
+            "name": "Australian Dollars",
+            "sign": "$"
+        }
+        ,
+        {
+            "code": "NZD",
+            "sellRate": 0.0475,
+            "name": "New Zealand Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "JPY",
+            "sellRate": 3.5617,
+            "name": "Japanese Yen",
+            "sign": "¥"
+        },
+        {
+            "code": "GBP",
+            "sellRate": 0.0246,
+            "name": "British Pounds",
+            "sign": "£"
+        }
+    ]
+}
+
+export default data;
